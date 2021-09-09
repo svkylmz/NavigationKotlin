589bd3853a987283385a68f9f085f46a18c35833
svkylmz <Ylmazsevket@gmail.com>
2021-09-09 17:35:35 +0300

    Initial commit

---
+++ app/src/main/java/com/svkylmz/navigationkotlin/FirstFragment.kt
@@ -0,0 +1,31 @@
+package com.svkylmz.navigationkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // todos after the fragment is created in onCreateView
+        buttonNext.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("svkylmz")
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
